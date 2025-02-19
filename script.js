9a27a6f843024ce54e5da39db19f3e0954f1c670
Developer Rahul <dboy1091997@gmail.com>
2025-02-19 13:46:09 +0530

    final-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+  Where's the JS? 
+  I used ES6, Webpack & modules.
+  Therefore, the JS had to be compiled 
+  and loaded through the asset manager.
+  Click the Source to see the JS code. 
+  Or go here:
+  https://github.com/winkerVSbecks/hookes-law
+*/
